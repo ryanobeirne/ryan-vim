61c064679bf105b9d53763f0d91a32e92e9c1f43
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2020-05-25 19:38:29 -0400

    Make links interactive

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -e
 
 # Link repo files in home directory
 rm -ri ~/{.vim,.vimrc} || :
-ln -s "$(realpath "$(dirname "$0")")/.vim" ~/
-ln -s "$(realpath "$(dirname "$0")")/.vimrc" ~/
+ln -si "$(realpath "$(dirname "$0")")/.vim" ~/
+ln -si "$(realpath "$(dirname "$0")")/.vimrc" ~/
 
 # Make undo directory
 [[ -d ~/.vimundodir ]] || mkdir ~/.vimundodir
